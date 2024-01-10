93b8b484a84221b722d697b24a02042966fee8a3
ben.wisnet <ben.wisnet@gmail.com>
2024-01-10 00:33:44 +0000

    Handle ws action

--- shed_server.py
+++ shed_server.py
@@ -11,6 +11,7 @@ class Lobby:
     def __init__(self, name: str):
         self.name = name
         self.players: Dict[str, WebSocket] = {}
+        self.ready = Dict[str, bool] = {}
 
 lobbies: Dict[str, Lobby] = {}
 
@@ -45,14 +46,56 @@ async def join_lobby(websocket: WebSocket, lobby_name: str, player_name: str):
     await websocket.accept()
     try:
         lobbies[lobby_name].players[player_name] = websocket
+        lobbies[lobby_name].ready[player_name] = False
         while True:
-            logging.info("yo!")
             data = await websocket.receive_text()
             logging.info(data)
-            # Handle incoming data
-            # ...
+            try:
+                # Handle incoming data
+                action = parse_action(data)
+                await handle_action(action, websocket, lobby_name, player_name)
+                # ...
+            except ValueError as err:
+                # websocket.send_json type: error, value: err
+                print(f"Action Value Error: {err}")
     except WebSocketDisconnect:
         del lobbies[lobby_name].players[player_name] # Remove disconnected player
+        del lobbies[lobby_name].ready[player_name]
         # Reconnect attempt needed first?
         if not lobbies[lobby_name].players:
             del lobbies[lobby_name]  # Delete lobby if empty
+
+def parse_action(message: str):
+    try:
+        data = json.loads(message)
+        if 'type' in data:
+            return data
+        else:
+            raise ValueError("Message does not contain an action type")
+    except json.JSONDecodeError:
+        raise ValueError("Invalid JSON message")
+
+async def handle_action(action, websocket, lobby_name, player_name):
+    if action['type'] == "ready_up":
+        lobbies[lobby_name].ready[player_name] = True
+        message = {
+            "type": "ready_status",
+            "player": player_name,
+            "is_ready": True
+        }
+        await broadcast_message(lobby_name, player_name, message)
+    elif action['type'] == "ready_down":
+        lobbies[lobby_name].ready[player_name] = True
+        message = {
+            "type": "ready_status",
+            "player": player_name,
+            "is_ready": False
+        }
+        await broadcast_message(lobby_name, player_name, message)
+
+async def broadcast_message(lobby_name: str, player_name: str, message: dict):
+    if lobby_name in lobbies:
+        json_string = json.dumps(message)
+        for player, websocket in lobbies[lobby_name].players.items():
+            if player != player_name:
+                await websocket.send_text(json_string)
